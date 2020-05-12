02ea4ceb550cb4c6e042f131a90e0de44197a3b7
IPogorelova <pisseversk@mail.ru>
2020-05-12 09:35:09 +0300

    add css-vars-polyfill for IE

--- app/index.entry.js
+++ app/index.entry.js
@@ -3,6 +3,15 @@ import './exterrnal-libs/layout/index.css'
 import './index.entry.pcss';
 import "./exterrnal-libs/breakpoints/index.css";
 
+import cssVars from 'css-vars-ponyfill';
+
+cssVars({
+  onlyLegacy: false,
+  preserveStatic: false,
+  preserveVars: true,
+  shadowDOM: true,
+});
+
 document.addEventListener('DOMContentLoaded', () => {
 
 });
